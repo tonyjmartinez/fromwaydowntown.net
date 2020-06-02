84949a54ed81671debebbf3a57450f29e0e25768
Tony Martinez <ajosephmartinez@gmail.com>
2020-06-02 11:32:23 -0700

    .

--- web/src/components/Toggle/Toggle.js
+++ web/src/components/Toggle/Toggle.js
@@ -51,17 +51,24 @@ const CheckBox = styled.input`
 `
 const Toggle = ({ value, toggleValue }) => {
   return (
-    <div>
-      <CheckBoxWrapper>
-        <CheckBox
-          id="checkbox"
-          type="checkbox"
-          onChange={toggleValue}
-          checked={value}
+    <button
+      onClick={toggleValue}
+      className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        className="fill-current w-4 h-4 mr-2"
+      >
+        <path
+          className="heroicon-ui"
+          d="M9.57 3.38a8 8 0 0 0 10.4 10.4 1 1 0 0 1 1.31 1.3 10 10 0 1 1-13-13 1 1 0 0 1 1.3 1.3zM7.1 5.04A8 8 0 1 0 18.3 16.27 10 10 0 0 1 7.08 5.04z"
         />
-        <CheckBoxLabel htmlFor="checkbox" />
-      </CheckBoxWrapper>
-    </div>
+      </svg>
+      <span>{value ? 'Dark' : 'Light'}</span>
+    </button>
   )
 }
 
